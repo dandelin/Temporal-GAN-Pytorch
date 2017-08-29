a318074d14a81bcbffc6df431e301470b90c00a1
Wonjae Kim <wjkim@hcil.snu.ac.kr>
2017-08-29 17:48:56 +0900

    TGIF loader

--- loader.py
+++ loader.py
@@ -1,5 +1,9 @@
 import numpy as np
-from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import torch
+from torch.utils.data import Dataset, DataLoader
+from glob import glob
+from PIL import Image, ImageSequence
 
 class MovingMNIST(Dataset):
     def __init__(self, path='mnist_test_seq.npy'):
@@ -12,3 +16,43 @@ class MovingMNIST(Dataset):
         ret = (ret / 255) * 2 - 1
         # C x D x H x W
         return np.expand_dims(ret, axis=0)
+
+class GIF(Dataset):
+    def __init__(self, path='gifs'):
+        self.paths = glob(f'{path}/*.gif')
+    def __len__(self):
+        return len(self.paths)
+    def __getitem__(self, index):
+        path = self.paths[index]
+        gif = Image.open(path)
+        gif_iter = ImageSequence.Iterator(gif)
+        frames = list(gif_iter)
+
+        f = lambda m, n: [i*n//m + n//(2*m) for i in range(m)]
+        indice = f(16, len(frames))
+        frames = [frames[i] for i in indice]
+
+        tensors = []
+        for frame in frames:
+            image = frame.convert('RGB')
+            h, w = image.size
+            hpad = (max(h, w) - h) // 2
+            wpad = (max(h, w) - w) // 2
+            pad = (hpad, wpad, hpad, wpad)
+            transform = transforms.Compose([
+                transforms.Pad(pad),
+                transforms.Scale([64, 64]),
+                transforms.ToTensor()
+            ])
+            image_tensor = transform(image)
+            tensors.append(image_tensor)
+        tensors = torch.stack(tensors)
+        tensors = torch.transpose(tensors, 0, 1)
+        tensors = tensors * 2 - 1
+        return tensors
+
+if __name__ == '__main__':
+    dset = GIF()
+    loader = DataLoader(dset, batch_size=2)
+    for data in loader:
+        print(data)
